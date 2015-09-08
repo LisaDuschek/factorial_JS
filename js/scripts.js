17700764daf018570db52d334732a4b474bf5f56
Lisa Duschek & Lisa Folliard <student@epicodus.com>
2015-09-08 14:45:26 -0700

    calculate a factorial

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var factorial = function(number) {
+
+  var result = 1;
+  for (var counter = number; counter > 0; counter -=1) {
+
+  result = result*counter;
+  }
+
+  return result;
+
+};
+
+
+$(document).ready(function() {
+  
+
+
+});
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('factorial', function() {
+  it("takes an input-number and multiplies it by its integers less than that number", function() {
+    expect(factorial(5)).to.equal(120);
+  });
+
+});
